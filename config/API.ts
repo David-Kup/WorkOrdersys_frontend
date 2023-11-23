ec47fa2f8b9e52a0c736bd6377eac352acb89e35
Natural <128178042+naturalspirit-dev@users.noreply.github.com>
2023-11-24 02:25:14 +0800

    Update API.ts
    add credentials: 'include' for cookie

--- config/API.ts
+++ config/API.ts
@@ -3,5 +3,6 @@ export const SERVER_URL = process.env.SERVER_URL || 'http://192.168.1.129:6060'
 import { extend } from "umi-request";
 
 export const request = extend({
-    prefix: SERVER_URL, // Set your default prefix URL here
-  });
+  prefix: SERVER_URL,
+  credentials: 'include', // 'include' sends cookies for cross-origin requests
+});
